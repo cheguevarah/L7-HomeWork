dd5d4b2966967bdba685d9fb430baa7aff3d1d1a
s.) <sashkah@icloud.com>
2019-02-14 16:27:52 +0200

    L7 HomeWork, almost all, without saving history in UserDefaults

--- L7-HomeWork/ViewController.swift
+++ L7-HomeWork/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
+var globalResult: Int = 0
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        resultLabel.text = UserDefaults.standard.string(forKey: "result")
+        
+        
     }
+    
+    
+   
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var topField: UITextField!
+    
+    @IBOutlet weak var bottomField: UITextView!
+    
+    
+    
+    
+    @IBAction func addNumButton() {
+        let firstLabel = topField.text
+        let intFirstLabel = Int(firstLabel ?? "0")
+        let secondLabel = bottomField.text
+        let intSecondLabel = Int(secondLabel ?? "0")
+        let result = (intFirstLabel ?? 0) + (intSecondLabel ?? 0)
+        globalResult = result
+        let stringGlobalRes = String(globalResult)
+        resultLabel.text = stringGlobalRes
+        UserDefaults.standard.set(stringGlobalRes, forKey: "result")
+        bottomField.font = bottomField.font?.withSize(CGFloat(globalResult))
+        
+    }
+    
+    
+    
+    
+    
+    
 
 
-}
+
+    }
+    
+
+
 
